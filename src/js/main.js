112e1a96ad6a0686f2f6b41f1d0d9c7dadf455a4
dq212 <>
2019-10-14 11:17:05 -0400

    ok

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+    //Get modal element
+    var modal = document.getElementById('simpleModal');
+
+    // //Get open modal button
+    var modalBtn = document.getElementById('modalBtn');
+    // //Get close button
+    var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+    var modalContent = document.getElementById('modalBodyCopy');
+    
+    // //Listen for click
+    // modalBtn.addEventListener('click', openModal);
+
+    closeBtn.addEventListener('click', closeModal);
+
+    window.addEventListener('click', clickOutside);
+
+    // //Function to open modal
+    function openModal(e, txt="") {
+        var str = txt;
+        modal.style.display = 'block';
+       
+        modalContent.innerHTML = str;
+      
+        }
+    
+    function closeModal() {
+        // console.log(123);
+        modal.style.display = 'none';
+    }
+
+    function clickOutside(e) {
+        if (e.target == modal)
+        modal.style.display = 'none';
+    }
